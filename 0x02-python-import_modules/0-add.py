10bd253a039ee4c02f861b446dc2ee6eb50e15e8
Lulusha-XT <leulsileshi7@gmail.com>
2022-09-22 23:31:50 +0300

    2-args.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add
-
-
-def main():
+if __name__ == "__main__":
+    from add_0 import add
     a = 1
     b = 2
-    print("{0:d} + {1:d} = {2:d}".format(a, b, add(a, b)))
-
-if __name__ == "__main__":
-    main()
+    print("{} + {} = {}".format(a, b, add(a, b)))

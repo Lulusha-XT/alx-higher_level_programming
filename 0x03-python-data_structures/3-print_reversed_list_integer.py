daae181687e87321beeb7601170a7615d2757942
Lulusha-XT <leulsileshi7@gmail.com>
2022-09-27 23:23:15 +0300

    3-print_reversed_list_integer.py

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    if my_list is None:
+        return None
+    for number in my_list[::-1]:
+        print("{:d}".format(number))

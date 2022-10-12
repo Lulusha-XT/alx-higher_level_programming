1d9032aab8e4787b6cb7de6a401406a28bbb4d75
Lulusha-XT <leulsileshi7@gmail.com>
2022-10-13 01:12:27 +0300

    102-main.py

---
+++ 0x06-python-classes/102-main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+square = __import__('102-square').Square
+
+s_5 = Square(5)
+s_6 = Square(6)
+
+if s_5 < s_6:
+    print("Square 5 < Square 6")
+if s_5 <= s_6:
+    print("Square 5 <= Square 6")
+if s_5 == s_6:
+    print("Square 5 == Square 6")
+if s_5 != s_6:
+    print("Square 5 != Square 6")
+if s_5 > s_6:
+    print("Square 5 > Square 6")
+if s_5 >= s_6:
+    print("Square 5 >= Square 6")
